df0b5025fb5afab01aae58017f66924efe37a68d
Thaneshwara848 <69508078+Thaneshwara848@users.noreply.github.com>
2020-09-21 20:48:01 +0530

    COde commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlightsComponent } from './flights/flights.component';
+import {ApiServiceService} from './api-service.service'
+import { from } from 'rxjs';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule  
   ],
-  providers: [],
+  providers: [ApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
